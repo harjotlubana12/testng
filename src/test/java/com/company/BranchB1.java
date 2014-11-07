d3a2b26078d5dbfbc478a859029e5c847c87f75a
Harjot Singh <harjot.singh@gspann.com>
2014-11-07 12:02:13 +0530

    from Branch B1

---
+++ src/test/java/com/company/BranchB1.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import org.testng.annotations.Test;
+
+public class BranchB1 {
+
+	@Test
+	public void branchTest() {
+		System.out.println("<<<<<<<<<<<<<<<<------------Branch Test----------->>>>>>>>>>>>");
+	}
+}
